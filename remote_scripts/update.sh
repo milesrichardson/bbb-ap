d6921daf04134939d5f99ceee339c4d66c9c0140
Miles Richardson <milesrichardson@gmail.com>
2015-04-03 18:24:42 -0400

    update.sh updates

--- remote_scripts/update.sh
+++ remote_scripts/update.sh
@@ -13,14 +13,16 @@ cp configs/etc/rc.local /etc/rc.local
 
 # Deps
 cd build
-git clone http://github.com/milesrichardson/mt7601u
-
-# mt7601u: Unload modules, build and deploy again
-cd mt7601u
-sh ~/bbb-ap/build/mt7601u/unload.sh
 
 if [ "$1" == "build" ]; then
+    rm -rf mt7601u
+    git clone http://github.com/milesrichardson/mt7601u
+    sh ~/bbb-ap/build/mt7601u/unload.sh
     sh ~/bbb-ap/build/mt7601u/miwifi_build.sh
 fi
 
+# mt7601u: Unload modules, build and deploy again
+cd mt7601u
+ifdown ra0
+sh ~/bbb-ap/build/mt7601u/unload.sh
 sh ~/bbb-ap/build/mt7601u/miwifi_work.sh
