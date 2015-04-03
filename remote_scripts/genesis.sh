453dc898119e2e14c422e3191bce4601a399c0eb
Miles Richardson <milesrichardson@gmail.com>
2015-04-02 21:22:41 -0400

    update include module unload etc

--- remote_scripts/genesis.sh
+++ remote_scripts/genesis.sh
@@ -10,6 +10,7 @@ apt-get upgrade
 
 # Necessary for building kernel packages
 apt-get install -y linux-headers-$(uname-r)
+apt-get install -y udhcpd
 
 # Create (and delete if exists) buildroot at ~/bbb-ap
 cd ~
@@ -20,3 +21,8 @@ cd bbb-ap
 # Load deps into ~/bbb-ap/build
 cd build
 git clone https://github.com/milesrichardson/mt7601u
+
+# Run update script
+cd ../remote_scripts
+
+./update.sh
--- remote_scripts/update.sh
+++ remote_scripts/update.sh
@@ -14,3 +14,9 @@ git pull
 # Deps
 cd build
 git clone http://github.com/milesrichardson/mt7601u
+
+# mt7601u: Unload modules, build and deploy again
+cd mt7601u
+./unload.sh
+./miwifi_build.sh
+./miwifi_work.sh
